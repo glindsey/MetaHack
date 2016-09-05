341d5e719d08ff85d42ac6ea5faaffc4954f2e8d
Gregory Lindsey <greg@lindseyweb.net>
2016-09-05 15:31:25 -0500

    Minor Lua script fixes

--- resources/things/Marcher.lua
+++ resources/things/Marcher.lua
@@ -10,5 +10,10 @@ Marcher.intrinsics.creatable = true
 Marcher.intrinsics.maxhp = range(1, 1)
 
 function Marcher.get_brief_description()
-	return "A testing Entity that endlessly marches left and right."
+    return "A testing Entity that endlessly marches left and right."
+end
+
+function Marcher.process()
+    print "DEBUG: Marcher.process() called."
+    return ActionResult.Success
 end
